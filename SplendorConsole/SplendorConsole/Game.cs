408138f5a9f0ddf1da00f3bdcf9d15b38869019b
DjCr4y0n <cray.kox1234@gmail.com>
2024-10-24 16:25:46 +0200

    Stworzona klasę Game oraz dodane pierwsze dwie metody
    Na razie tylko tyle, by móc pracować z Witkiem

---
+++ SplendorConsole/SplendorConsole/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplendorConsole
+{
+    internal class Game
+    {
+        public void Start()
+        {
+
+        }
+
+        public Player SetNumberOfPlayers()
+        {
+
+        }
+    }
+}
