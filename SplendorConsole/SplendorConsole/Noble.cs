33745b630a44def6961016898bb237598e3bb260
khomziuk <kamil.homziuk@op.pl>
2024-11-04 21:14:04 +0100

    [feature/86968fzd4] dodanie właściwości Points i RequiredBonuses oraz implementacja IEnumerable<Resources>

--- SplendorConsole/SplendorConsole/Noble.cs
+++ SplendorConsole/SplendorConsole/Noble.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,31 @@ using System.Threading.Tasks;
 
 namespace SplendorConsole
 {
-    public class Noble
+    public class Noble:IEnumerable<Resources>
     {
         private int points;
+        public int Points
+        {
+            get => points;
+            set => points = value;
+        }
         private Resources requiredBonuses;
+
+        public Resources RequiredBonuses
+        {
+            get=> requiredBonuses;
+            set => requiredBonuses = value;
+        }
         private string illustration;
+
+        public IEnumerator<Resources> GetEnumerator()
+        {
+            return (IEnumerator<Resources>)requiredBonuses.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
