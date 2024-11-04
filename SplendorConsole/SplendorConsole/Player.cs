d23274e7910c01f37597e583da5c9a36e2f3aee3
albertgmi <gmitrzak.albert@gmail.com>
2024-11-04 19:09:23 +0100

    Implemented CanAffordCard
    Implemented CanAffordCard and added WantToSpendGoldCoin

--- SplendorConsole/SplendorConsole/Player.cs
+++ SplendorConsole/SplendorConsole/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +10,22 @@ namespace SplendorConsole
     internal class Player
     {
         private Resources resources = new Resources();
-        private Resources bonusResources;
+        private Resources bonusResources = new Resources();
         private Card[] hand;
         private Card[] reservedCard;
         private Noble[] nobles;
         private int points;
+
+        public Player(Resources resources, Resources bonusResources, Card[] hand, Card[] reservedCard, Noble[] nobles, int points)
+        {
+            this.resources = resources;
+            this.bonusResources = bonusResources;
+            this.hand = hand;
+            this.reservedCard = reservedCard;
+            this.nobles = nobles;
+            this.points = points;
+        }
+
         public void BuyCard(Card card)
         {
             throw new NotImplementedException();
@@ -54,12 +66,54 @@ namespace SplendorConsole
         }
         public bool CanAffordCard(Card card)
         {
-            throw new NotImplementedException();
+            var price = card.DetailedPrice.gems;
+            var playerResources = this.resources.gems;
+            var playerBonuses = this.bonusResources.gems;
+            var wantToSpendGoldCoins = WantToSpendGoldCoin();
+
+            int goldTokens = playerResources.ContainsKey(GemColor.GOLDEN) ? playerResources[GemColor.GOLDEN] : 0;
+
+            foreach (var colorOnCard in price)
+            {
+                GemColor color = colorOnCard.Key;
+                int requiredAmount = colorOnCard.Value;
+
+                int playerAmount = playerResources.ContainsKey(color) ? playerResources[color] : 0;;
+                int bonusAmount = playerBonuses.ContainsKey(color) ? playerBonuses[color] : 0;
+                int totalPlayersCoins = playerAmount + bonusAmount;
+
+                if (wantToSpendGoldCoins && totalPlayersCoins<requiredAmount)
+                {
+                    int deficit = requiredAmount - totalPlayersCoins;
+                    if (goldTokens>=deficit)
+                        goldTokens -=deficit;
+                    else
+                        return false;
+                }
+                if (!wantToSpendGoldCoins && totalPlayersCoins < requiredAmount)
+                    return false;
+            }
+            return true;
         }
         public void PassTurn()
         {
             throw new NotImplementedException();
         }
-
+        public bool WantToSpendGoldCoin()
+        {
+            Console.WriteLine("Czy chcesz użyć złotego żetonu aby płacić za kartę?");
+            Console.WriteLine("1 - Tak");
+            Console.WriteLine("2 - Nie");
+            var wantTo = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                if (wantTo == 2)
+                    return false;
+                else if (wantTo == 1)
+                    return true;
+                else
+                    Console.WriteLine("Podano zły klawisz. 1 lub 2");
+            }
+        }
     }
 }
