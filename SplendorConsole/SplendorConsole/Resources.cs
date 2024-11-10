bc91ea2c3a972b24ef721815a485551fc6b365f0
bartekA05 <bartadamo2005@gmail.com>
2024-11-10 16:14:26 +0100

    Revert "feature/8696jjk3q - edycja ToStringa w resources"
    This reverts commit 7d43ef8c3a93048493aabdc82889725f272100b1.

--- SplendorConsole/SplendorConsole/Resources.cs
+++ SplendorConsole/SplendorConsole/Resources.cs
@@ -39,19 +39,10 @@ namespace SplendorConsole
             }
             return hash;
         }
-        
         public override string ToString()
         {
-            int i = 1;
-            string gemsdate = "";
-            foreach (var singlegem in gems)
-            {
-                gemsdate += $"{i}. {singlegem.Key}: {singlegem.Value}\n";
-                i++;
-            }
-            return gemsdate;
+            return string.Join(", ", gems.Select(gem => $"{gem.Key}: {gem.Value}"));
         }
-        
         public void AddResource(GemColor color)
         {
             if (gems.ContainsKey(color))
