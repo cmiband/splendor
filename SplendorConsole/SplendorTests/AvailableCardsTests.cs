c53aa0c2ae5f8f17064a698259afa94bfb4f5fd2
xKsOm <adam.domzalowicz@gmail.com>
2024-11-17 13:02:59 +0100

    [feature/869fx63a]

--- SplendorConsole/SplendorTests/AvailableCardsTests.cs
+++ SplendorConsole/SplendorTests/AvailableCardsTests.cs
@@ -14,75 +14,75 @@ namespace SplendorTests
         [Test]
         public void AddCard_Level1Card_ShouldAdd()
         {
-            // Arrange
+            
             var availableCards = new AvailableCards();
             var detailedPrice = new Resources();
             detailedPrice.gems[GemColor.WHITE] = 1;
             var card = new Card(1, GemColor.WHITE, 2, "1", detailedPrice);
 
-            // Act
+            
             availableCards.AddCard(card);
 
-            // Assert
+            
             Assert.That(availableCards.level1Cards.Count(), Is.EqualTo(1));
         }
         [Test]
         public void AddCard_Level2Card_ShouldAdd()
         {
-            // Arrange
+            
             var availableCards = new AvailableCards();
             var detailedPrice = new Resources();
             detailedPrice.gems[GemColor.WHITE] = 1;
             var card = new Card(2, GemColor.WHITE, 2, "1", detailedPrice);
 
-            // Act
+            
             availableCards.AddCard(card);
 
-            // Assert
+            
             Assert.That(availableCards.level2Cards.Count(), Is.EqualTo(1));
         }
         [Test]
         public void AddCard_Level3Card_ShouldAdd()
         {
-            // Arrange
+            
             var availableCards = new AvailableCards();
             var detailedPrice = new Resources();
             detailedPrice.gems[GemColor.WHITE] = 1;
             var card = new Card(3, GemColor.WHITE, 2, "1", detailedPrice);
 
-            // Act
+            
             availableCards.AddCard(card);
 
-            // Assert
+            
             Assert.That(availableCards.level3Cards.Count(), Is.EqualTo(1));
         }
         [Test]
         public void AddCard_ShouldNotAdd()
         {
-            // Arrange
+            
             var availableCards = new AvailableCards();
             var detailedPrice = new Resources();
             detailedPrice.gems[GemColor.WHITE] = 1;
             var card = new Card(4, GemColor.WHITE, 2, "1", detailedPrice);
 
-            // Act
+            
             var exception = Assert.Throws<ArgumentException>(() => availableCards.AddCard(card));
 
-            // Assert
+            
             Assert.That(exception.Message, Is.EqualTo("Wrong card level!"));
         }
         [Test]
         public void LoadCardsFromExcel_Test()
         {
-            // Arrange
+            
             var availableCards = new AvailableCards();
 
-            // Act
+            
             availableCards.LoadCardsFromExcel("LoadCardsFromExcelTest.xlsx");
             var card = availableCards.level3Cards[0].ToString();
             string message = "Karta koloru: WHITE,\t cena: WHITE: 1, BLUE: 2, GREEN: 3, RED: 4, BLACK: 5,\t dodająca 1 punktów.";
 
-            // Assert
+            
             Assert.That(card, Is.EqualTo(message));
         }
     }
--- SplendorConsole/SplendorTests/BankTests.cs
+++ SplendorConsole/SplendorTests/BankTests.cs
@@ -16,71 +16,71 @@ namespace SplendorTests
         [Test]
         public void TakeOutResources_ShouldTakeOut()
         {
-            // Arrange
+            
             var _Bank = new Bank();
             _Bank.resources.gems[GemColor.WHITE] = 3;
 
-            // Act
+            
             int remove = 2;
             _Bank.TakeOutResources(remove, GemColor.WHITE);
 
-            // Assert
+            
             Assert.That(_Bank.resources.gems[GemColor.WHITE], Is.EqualTo(1));
         }
 
         [Test]
         public void AddResources_ShouldAdd()
         {
-            // Arrange
+            
             var _Bank = new Bank();
             _Bank.resources.gems[GemColor.BLUE] = 3;
 
-            // Act
+            
             int add = 4;
             _Bank.AddResources(add, GemColor.BLUE);
 
-            // Assert
+            
             Assert.That(_Bank.resources.gems[GemColor.BLUE], Is.EqualTo(3+add));     
         }
 
         [Test]
         public void AddGoldenGem_ShouldAdd()
         {
-            // Arrange
+            
             var _Bank = new Bank();
             _Bank.resources.gems[GemColor.GOLDEN] = 1;
 
-            // Act
+            
             _Bank.AddGoldenGem();
 
-            // Assert
+            
             Assert.That(_Bank.resources.gems[GemColor.GOLDEN], Is.EqualTo(2));
         }
 
         [Test]
         public void CanTakeGoldenGem_ReturnTrue()
         {
-            // Arrange
+            
             var _Bank = new Bank();
             _Bank.resources.gems[GemColor.GOLDEN] = 1;
 
-            // Act
+            
             bool canTake = _Bank.CanTakeGoldenGem();
 
-            // Assert
+            
             Assert.That(canTake, Is.EqualTo(true));
         }
         [Test]
         public void CanTakeGoldenGem_ReturnFalse()
         {
-            // Arrange
+            
             var _Bank = new Bank();
             _Bank.resources.gems[GemColor.GOLDEN] = 0;
 
-            // Act
+            
             bool canTake = _Bank.CanTakeGoldenGem();
 
-            // Assert
+            
             Assert.That(canTake, Is.EqualTo(false));
         }
     }
