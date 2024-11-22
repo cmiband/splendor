b6171243a46771a0e20cf16df58636f902d93b8c
xKsOm <adam.domzalowicz@gmail.com>
2024-11-22 11:44:32 +0100

    [feature/8696fx63a2]
    Added tests to Player class..

--- SplendorConsole/SplendorTests/BankTests.cs
+++ SplendorConsole/SplendorTests/BankTests.cs
@@ -17,55 +17,55 @@ namespace SplendorTests
         public void TakeOutResources_ShouldTakeOut()
         {
             
-            var _Bank = new Bank();
-            _Bank.resources.gems[GemColor.WHITE] = 3;
+            var Bank = new Bank();
+            Bank.resources.gems[GemColor.WHITE] = 3;
 
             
             int remove = 2;
-            _Bank.TakeOutResources(remove, GemColor.WHITE);
+            Bank.TakeOutResources(remove, GemColor.WHITE);
 
             
-            Assert.That(_Bank.resources.gems[GemColor.WHITE], Is.EqualTo(1));
+            Assert.That(Bank.resources.gems[GemColor.WHITE], Is.EqualTo(1));
         }
 
         [Test]
         public void AddResources_ShouldAdd()
         {
             
-            var _Bank = new Bank();
-            _Bank.resources.gems[GemColor.BLUE] = 3;
+            var Bank = new Bank();
+            Bank.resources.gems[GemColor.BLUE] = 3;
 
             
             int add = 4;
-            _Bank.AddResources(add, GemColor.BLUE);
+            Bank.AddResources(add, GemColor.BLUE);
 
             
-            Assert.That(_Bank.resources.gems[GemColor.BLUE], Is.EqualTo(3+add));     
+            Assert.That(Bank.resources.gems[GemColor.BLUE], Is.EqualTo(3+add));     
         }
 
         [Test]
         public void AddGoldenGem_ShouldAdd()
         {
             
-            var _Bank = new Bank();
-            _Bank.resources.gems[GemColor.GOLDEN] = 1;
+            var Bank = new Bank();
+            Bank.resources.gems[GemColor.GOLDEN] = 1;
 
             
-            _Bank.AddGoldenGem();
+            Bank.AddGoldenGem(1);
 
             
-            Assert.That(_Bank.resources.gems[GemColor.GOLDEN], Is.EqualTo(2));
+            Assert.That(Bank.resources.gems[GemColor.GOLDEN], Is.EqualTo(2));
         }
 
         [Test]
         public void CanTakeGoldenGem_ReturnTrue()
         {
             
-            var _Bank = new Bank();
-            _Bank.resources.gems[GemColor.GOLDEN] = 1;
+            var Bank = new Bank();
+            Bank.resources.gems[GemColor.GOLDEN] = 1;
 
             
-            bool canTake = _Bank.CanTakeGoldenGem();
+            bool canTake = Bank.CanTakeGoldenGem();
 
             
             Assert.That(canTake, Is.EqualTo(true));
@@ -74,11 +74,11 @@ namespace SplendorTests
         public void CanTakeGoldenGem_ReturnFalse()
         {
             
-            var _Bank = new Bank();
-            _Bank.resources.gems[GemColor.GOLDEN] = 0;
+            var Bank = new Bank();
+            Bank.resources.gems[GemColor.GOLDEN] = 0;
 
             
-            bool canTake = _Bank.CanTakeGoldenGem();
+            bool canTake = Bank.CanTakeGoldenGem();
 
             
             Assert.That(canTake, Is.EqualTo(false));
--- SplendorConsole/SplendorTests/PlayerTests.cs
+++ SplendorConsole/SplendorTests/PlayerTests.cs
@@ -14,89 +14,89 @@ namespace SplendorTests
         [Test]
         public void AddCardToPlayer()
         {
-            var _player = new Player();
+            var player = new Player();
 
-            var _detailedPrice = new Resources();
-            var _card = new Card(1, GemColor.WHITE, 2, "123", _detailedPrice);
+            var detailedPrice = new Resources();
+            var card = new Card(1, GemColor.WHITE, 2, "123", detailedPrice);
 
-            _player.AddCardToPlayer(_card);
+            player.AddCardToPlayer(card);
 
-            Assert.That(_player.hand.Count, Is.EqualTo(1)); 
+            Assert.That(player.hand.Count, Is.EqualTo(1)); 
         }
 
         [Test]
         public void CanAffordCard_ShouldFalse()
         {
-            var _player = new Player();
-            var _detailedPrice = new Resources();
-            _detailedPrice.gems.Add(GemColor.BLACK, 2);
-            var _card = new Card(1, GemColor.WHITE, 2, "123", _detailedPrice);
+            var player = new Player();
+            var detailedPrice = new Resources();
+            detailedPrice.gems.Add(GemColor.BLACK, 2);
+            var card = new Card(1, GemColor.WHITE, 2, "123", detailedPrice);
 
             bool test = false;
 
-            Assert.That(test,Is.EqualTo(_player.CanAffordCard(_card)));
+            Assert.That(test,Is.EqualTo(player.CanAffordCard(card)));
         }
 
         [Test]
         public void CanAffordCard_ShouldTrue()
         {
-            var _player = new Player();
-            var _detailedPrice = new Resources();
-            _detailedPrice.gems.Add(GemColor.BLACK, 2);
-            var _card = new Card(1, GemColor.WHITE, 2, "123", _detailedPrice);
+            var player = new Player();
+            var detailedPrice = new Resources();
+            detailedPrice.gems.Add(GemColor.BLACK, 2);
+            var card = new Card(1, GemColor.WHITE, 2, "123", detailedPrice);
 
-            _player.Resources.gems.Add(GemColor.BLACK, 2);
+            player.Resources.gems.Add(GemColor.BLACK, 2);
 
             bool test = true;
 
-            Assert.That(test, Is.EqualTo(_player.CanAffordCard(_card)));
+            Assert.That(test, Is.EqualTo(player.CanAffordCard(card)));
         }
         [Test]
         public void CanAffordCardWithGolden_ShouldFalse()
         {
-            var _player = new Player();
-            var _detailedPrice = new Resources();
-            _detailedPrice.gems.Add(GemColor.BLACK, 2);
-            var _card = new Card(1, GemColor.WHITE, 2, "123", _detailedPrice);
+            var player = new Player();
+            var detailedPrice = new Resources();
+            detailedPrice.gems.Add(GemColor.BLACK, 2);
+            var card = new Card(1, GemColor.WHITE, 2, "123", detailedPrice);
 
             bool test = false;
 
-            Assert.That(test, Is.EqualTo(_player.CanAffordCardWithGolden(_card)));
+            Assert.That(test, Is.EqualTo(player.CanAffordCardWithGolden(card)));
         }
         [Test]
         public void CanAffordCardWithGolden_ShouldTrue()
         {
-            var _player = new Player();
-            var _detailedPrice = new Resources();
-            _detailedPrice.gems.Add(GemColor.BLACK, 2);
-            var _card = new Card(1, GemColor.WHITE, 2, "123", _detailedPrice);
+            var player = new Player();
+            var detailedPrice = new Resources();
+            detailedPrice.gems.Add(GemColor.BLACK, 2);
+            var card = new Card(1, GemColor.WHITE, 2, "123", detailedPrice);
 
-            _player.Resources.gems.Add(GemColor.BLACK, 1);
-            _player.Resources.gems.Add(GemColor.GOLDEN, 1);
+            player.Resources.gems.Add(GemColor.BLACK, 1);
+            player.Resources.gems.Add(GemColor.GOLDEN, 1);
 
             bool test = true;
 
-            Assert.That(test, Is.EqualTo(_player.CanAffordCardWithGolden(_card)));
+            Assert.That(test, Is.EqualTo(player.CanAffordCardWithGolden(card)));
         }
         [Test]
         public void GetIndex()
         {
-            var _player = new Player();
-            var _detailedPrice = new Resources();
-            _detailedPrice.gems.Add(GemColor.BLACK, 2);
-            var _card = new Card(1, GemColor.WHITE, 2, "123", _detailedPrice);
-            var _card2 = new Card(2, GemColor.BLACK, 2, "321", _detailedPrice);
+            var player = new Player();
+            var detailedPrice = new Resources();
+            detailedPrice.gems.Add(GemColor.BLACK, 2);
+            var card = new Card(1, GemColor.WHITE, 2, "123", detailedPrice);
+            var card2 = new Card(2, GemColor.BLACK, 2, "321", detailedPrice);
 
-            Card[] test = new Card[] { _card, _card2, };
+            Card[] test = new Card[] { card, card2, };
 
-            Assert.That(1, Is.EqualTo(_player.GetIndex(_card2, test)));
+            Assert.That(1, Is.EqualTo(player.GetIndex(card2, test)));
         }
         [Test]
         public void PassTurn()
         {
-            var _player = new Player();
+            var player = new Player();
 
-            var exception = Assert.Throws<NotImplementedException>(() => _player.PassTurn());
+            var exception = Assert.Throws<NotImplementedException>(() => player.PassTurn());
 
             var test = new NotImplementedException();
 
@@ -105,70 +105,70 @@ namespace SplendorTests
         [Test]
         public void GetNoble()
         {
-            var _player = new Player();
-            var _detailedPrice = new Resources();
-            var _noble = new Noble(3, _detailedPrice);
+            var player = new Player();
+            var detailedPrice = new Resources();
+            var noble = new Noble(3, detailedPrice);
 
-            _player.GetNoble(_noble);
+            player.GetNoble(noble);
 
-            Assert.That(1, Is.EqualTo(_player.Nobles.Count));
+            Assert.That(1, Is.EqualTo(player.Nobles.Count));
         }
         [Test]
         public void ReserveCard()
         {
-            var _player = new Player();
-            var _detailedPrice = new Resources();
-            var _card = new Card(1, GemColor.WHITE, 2, "123", _detailedPrice);
+            var player = new Player();
+            var detailedPrice = new Resources();
+            var card = new Card(1, GemColor.WHITE, 2, "123", detailedPrice);
 
-            _player.ReserveCard(_card); 
+            player.ReserveCard(card); 
 
-            Assert.That(1, Is.EqualTo(_player.ReservedCards.Count));   
+            Assert.That(1, Is.EqualTo(player.ReservedCards.Count));   
         }
         [Test]
         public void TakeTwoTokens()
         {
-            var _player = new Player();
-            var _resources = new Resources();
-            _player.TakeTwoTokens(_resources, GemColor.WHITE);
+            var player = new Player();
+            var resources = new Resources();
+            player.TakeTwoTokens(resources, GemColor.WHITE);
 
-            Assert.That(2, Is.EqualTo(_player.Resources.gems[GemColor.WHITE]));    
+            Assert.That(2, Is.EqualTo(player.Resources.gems[GemColor.WHITE]));    
         }
         [Test]
         public void TakeThreeTokens()
         {
-            var _player = new Player();
-            var _resources = new Resources();
+            var player = new Player();
+            var resources = new Resources();
             GemColor[] gemColor = new GemColor[] { GemColor.BLUE, GemColor.WHITE, GemColor.RED };
-            _player.TakeThreeTokens(_resources, gemColor);
+            player.TakeThreeTokens(resources, gemColor);
 
-            Assert.That(1, Is.EqualTo(_player.Resources.gems[GemColor.WHITE]));
-            Assert.That(1, Is.EqualTo(_player.Resources.gems[GemColor.BLUE]));
-            Assert.That(1, Is.EqualTo(_player.Resources.gems[GemColor.RED]));
+            Assert.That(1, Is.EqualTo(player.Resources.gems[GemColor.WHITE]));
+            Assert.That(1, Is.EqualTo(player.Resources.gems[GemColor.BLUE]));
+            Assert.That(1, Is.EqualTo(player.Resources.gems[GemColor.RED]));
         }
         [Test]
         public void RemoveOneToken_ShouldRemove()
         {
-            var _player = new Player();
-            _player.Resources.gems[GemColor.WHITE] = 2;
+            var player = new Player();
+            player.Resources.gems[GemColor.WHITE] = 2;
 
-            _player.RemoveOneToken(_player.Resources, GemColor.WHITE);
+            player.RemoveOneToken(player.Resources, GemColor.WHITE);
 
-            Assert.That(1, Is.EqualTo(_player.Resources.gems[GemColor.WHITE]));
+            Assert.That(1, Is.EqualTo(player.Resources.gems[GemColor.WHITE]));
         }
         [Test]
         public void PointsCounter()
         {
-            var _player = new Player();
-            var _detailedPrice = new Resources();
-            _detailedPrice.gems.Add(GemColor.BLACK, 2);
-            var _card = new Card(1, GemColor.WHITE, 2, "123", _detailedPrice);
-            var _noble = new Noble(3, _detailedPrice);
-            _player.hand.Add(_card);
-            _player.Nobles.Add(_noble); 
+            var player = new Player();
+            var detailedPrice = new Resources();
+            detailedPrice.gems.Add(GemColor.BLACK, 2);
+            var card = new Card(1, GemColor.WHITE, 2, "123", detailedPrice);
+            var noble = new Noble(3, detailedPrice);
+            player.hand.Add(card);
+            player.Nobles.Add(noble); 
 
-            _player.PointsCounter();
+            player.PointsCounter();
 
-            Assert.That(5, Is.EqualTo(_player.Points));
+            Assert.That(5, Is.EqualTo(player.Points));
         }
     }
 }
