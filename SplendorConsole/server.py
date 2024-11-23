63c3412f33ea5b332e91bfe688c0f6ae6a4b951a
Dominik Banach <dominikbanach5@gmail.com>
2024-11-23 03:06:46 +0100

    [8696jqz6p] Python server py + exe

--- SplendorConsole/server.py
+++ SplendorConsole/server.py
@@ -1,25 +1,27 @@
 import asyncio
 import websockets
 import json
-from random import randint
+from random import shuffle
 
 async def handle_connection(websocket: websockets.WebSocketServerProtocol, path: str) -> None:
     """Handles the connection from a client."""
     try:
         async for message in websocket:
-            print(f"Otrzymano wiadomość od klienta: {message}")
+            # print(f"Otrzymano wiadomość od klienta: {message}")
 
             data = json.loads(message)
-            print(f"Otrzymano dane: {data}")
+            # print(f"Otrzymano dane: {data}")
+
+            output = [i for i in range(1, 44)]
+            shuffle(output)
 
             response_object = {
-                "message": randint(1, 43),
-                "received": data
+                "MovesList": output,
             }
             response_json = json.dumps(response_object)
 
             await websocket.send(response_json)
-            print(f"Wysłano odpowiedź: {response_json}")
+            #print(f"Wysłano odpowiedź: {response_json}")
 
     except websockets.exceptions.ConnectionClosedError as e:
         print(f"Połączenie zamknięte: {e}")
@@ -28,7 +30,7 @@ async def handle_connection(websocket: websockets.WebSocketServerProtocol, path:
 
 async def start_server() -> None:
     """Starts the WebSocket server."""
-    async with websockets.serve(handle_connection, "localhost", 8765):
+    async with websockets.serve(handle_connection, "localhost", 8765, ping_timeout=None):
         print("Serwer WebSocket działa na ws://localhost:8765")
         await asyncio.Future()
 
