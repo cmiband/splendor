2b3560f7489d664df9607964cf805db272ee04b9
albertgmi <gmitrzak.albert@gmail.com>
2024-11-24 22:13:37 +0100

    Two functionalities added
    'None' Bug fixed, checking if player can afford card functionality added.

--- przepych/Assets/Scripts/PlayerController.cs
+++ przepych/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
@@ -35,50 +33,70 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-    
+
     public void HandleBuyCard()
     {
         int cardLevel = mainGameController.selectedToBuyCard.level;
         PlayerController player = mainGameController.currentPlayer.GetComponent<PlayerController>();
         Vector3 vector = mainGameController.selectedToBuyCard.transform.position;
+
+        if (!CanAffordCardWithGolden(mainGameController.selectedToBuyCard) && !CanAffordCard(mainGameController.selectedToBuyCard))
+        {
+            Debug.Log("Nie staæ ciê na tê kartê!");
+            return;
+        }
+
+        var copiedCard = CloneCard();
+        player.hand.Add(copiedCard);
+
         switch (cardLevel)
         {
             case 1:
-                {
-                    player.hand.Add(mainGameController.selectedToBuyCard);
-                    mainGameController.boardController.level1VisibleCardControllers.Remove(mainGameController.selectedToBuyCard);
-                    Debug.Log("Kupiono karte 1 poziomu");
-                    Destroy(mainGameController.selectedToBuyCard.gameObject);
-                    GameObject gameObject = Instantiate(mainGameController.boardController.cardPrefab, vector, Quaternion.identity, mainGameController.boardController.level1VisibleCards.transform);
-                    CardController cardController = gameObject.GetComponent<CardController>();
-                    cardController.InitCardData(mainGameController.boardController.level1StackController.PopCardFromStack());
-                    break;
-                }
-            case 2:
-                {
-                    player.hand.Add(mainGameController.selectedToBuyCard);
-                    mainGameController.boardController.level2VisibleCardControllers.Remove(mainGameController.selectedToBuyCard);
-                    Debug.Log("Kupiono karte 2 poziomu");
-                    Destroy(mainGameController.selectedToBuyCard.gameObject);
-                    GameObject gameObject = Instantiate(mainGameController.boardController.cardPrefab, vector, Quaternion.identity, mainGameController.boardController.level2VisibleCards.transform);
-                    CardController cardController = gameObject.GetComponent<CardController>();
-                    cardController.InitCardData(mainGameController.boardController.level2StackController.PopCardFromStack());
-                    break;
-                }
-            case 3:
-                {
-                    player.hand.Add(mainGameController.selectedToBuyCard);
-                    mainGameController.boardController.level3VisibleCardControllers.Remove(mainGameController.selectedToBuyCard);
-                    Debug.Log("Kupiono karte 3 poziomu");
-                    Destroy(mainGameController.selectedToBuyCard.gameObject);
-                    GameObject gameObject = Instantiate(mainGameController.boardController.cardPrefab, vector, Quaternion.identity, mainGameController.boardController.level3VisibleCards.transform);
-                    CardController cardController = gameObject.GetComponent<CardController>();
-                    cardController.InitCardData(mainGameController.boardController.level3StackController.PopCardFromStack());
-                    break;
-                }
+                mainGameController.boardController.level1VisibleCardControllers.Remove(mainGameController.selectedToBuyCard);
+                Debug.Log("Kupiono kartê 1 poziomu");
+                Destroy(mainGameController.selectedToBuyCard.gameObject);
+                GameObject gameObject1 = Instantiate(mainGameController.boardController.cardPrefab, vector, Quaternion.identity, mainGameController.boardController.level1VisibleCards.transform);
+                gameObject1.name = "Card_Level_" + cardLevel;
+                CardController cardController1 = gameObject1.GetComponent<CardController>();
+                cardController1.InitCardData(mainGameController.boardController.level1StackController.PopCardFromStack());
+                AddCardClickListener(gameObject1, cardController1);
+                break;
 
+            case 2:
+                mainGameController.boardController.level2VisibleCardControllers.Remove(mainGameController.selectedToBuyCard);
+                Debug.Log("Kupiono kartê 2 poziomu");
+                Destroy(mainGameController.selectedToBuyCard.gameObject);
+                GameObject gameObject2 = Instantiate(mainGameController.boardController.cardPrefab, vector, Quaternion.identity, mainGameController.boardController.level2VisibleCards.transform);
+                gameObject2.name = "Card_Level_" + cardLevel;
+                CardController cardController2 = gameObject2.GetComponent<CardController>();
+                cardController2.InitCardData(mainGameController.boardController.level1StackController.PopCardFromStack());
+                AddCardClickListener(gameObject2, cardController2);
+                break;
+
+            case 3:
+                mainGameController.boardController.level3VisibleCardControllers.Remove(mainGameController.selectedToBuyCard);
+                Debug.Log("Kupiono kartê 3 poziomu");
+                Destroy(mainGameController.selectedToBuyCard.gameObject);
+                GameObject gameObject3 = Instantiate(mainGameController.boardController.cardPrefab, vector, Quaternion.identity, mainGameController.boardController.level3VisibleCards.transform);
+                gameObject3.name = "Card_Level_" + cardLevel;
+                CardController cardController3 = gameObject3.GetComponent<CardController>();
+                cardController3.InitCardData(mainGameController.boardController.level1StackController.PopCardFromStack());
+                AddCardClickListener(gameObject3, cardController3);
+                break;
         }
+
         mainGameController.ChangeTurn();
+    }
+
+    private void AddCardClickListener(GameObject cardGameObject, CardController cardController)
+    {
+        Button button = cardGameObject.GetComponent<Button>();
+        if (button == null)
+        {
+            button = cardGameObject.AddComponent<Button>();
+            button.targetGraphic = cardGameObject.GetComponent<Image>();
+        }
+        button.onClick.AddListener(() => mainGameController.SelectCard(cardController));
     }
 
     public void TakeTwoTokens(GemColor color)
@@ -153,4 +171,63 @@ public class PlayerController : MonoBehaviour
             Debug.Log($"Dodano bonusowy zasób: {bonusColor}");
         }
     }
+    private bool CanAffordCard(CardController cardToBuy)
+    {
+        var cardPrice = cardToBuy.DetailedPrice.gems;
+        var playerResources = this.resources.gems;
+        var playerBonuses = this.bonusResources.gems;
+
+        foreach (var colorOnCard in cardPrice)
+        {
+            GemColor color = colorOnCard.Key;
+            int requiredAmount = colorOnCard.Value;
+
+            int playerAmount = playerResources.ContainsKey(color) ? playerResources[color] : 0;
+            int bonusAmount = playerBonuses.ContainsKey(color) ? playerBonuses[color] : 0;
+            int totalPlayersCoins = playerAmount + bonusAmount;
+
+            if (totalPlayersCoins < requiredAmount)
+                return false;
+        }
+        return true;
+    }
+    private bool CanAffordCardWithGolden(CardController cardToBuy)
+    {
+        var cardPrice = cardToBuy.DetailedPrice.gems;
+        var playerResources = this.resources.gems;
+        var playerBonuses = this.bonusResources.gems;
+
+        int goldTokens = playerResources.ContainsKey(GemColor.GOLDEN) ? playerResources[GemColor.GOLDEN] : 0;
+
+        foreach (var colorOnCard in cardPrice)
+        {
+            GemColor color = colorOnCard.Key;
+            int requiredAmount = colorOnCard.Value;
+
+            int playerAmount = playerResources.ContainsKey(color) ? playerResources[color] : 0;
+            int bonusAmount = playerBonuses.ContainsKey(color) ? playerBonuses[color] : 0;
+            int totalPlayersCoins = playerAmount + bonusAmount;
+
+            if (totalPlayersCoins < requiredAmount)
+            {
+                int deficit = requiredAmount - totalPlayersCoins;
+                if (goldTokens >= deficit)
+                    goldTokens -= deficit;
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+    private CardController CloneCard()
+    {
+        GameObject cardObject = Instantiate(mainGameController.selectedToBuyCard.gameObject);
+
+        CardController clonedCard = cardObject.GetComponent<CardController>();
+
+        clonedCard.InitCardData(mainGameController.selectedToBuyCard);
+
+        return clonedCard;
+    }
+
 }
