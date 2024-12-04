a55680f2c8838b77b7b6817577b982a0c9029b22
bartekA05 <33941168+cmiband@users.noreply.github.com>
2024-12-04 12:37:58 +0100

    fixes

--- SplendorConsole/SplendorTests/obj/Debug/net6.0/SplendorTests.AssemblyInfo.cs
+++ SplendorConsole/SplendorTests/obj/Debug/net6.0/SplendorTests.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SplendorTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+349d445200464007395ef58daa8c8dd4fd281b48")]
-=======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dad54b98c7ab79aca880008eecd04b60e35e52bc")]
->>>>>>> release2.0
 [assembly: System.Reflection.AssemblyProductAttribute("SplendorTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SplendorTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
