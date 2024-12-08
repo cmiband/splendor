b617bc6e965bf967563bb0c020265198a96b3dbc
tcnx1 <crwarg@gmail.com>
2024-12-08 22:22:04 +0100

    Basic main menu
    Created basic main menu, with "New Game" and "Quit" buttons

--- przepych/Assets/Scripts/MainMenuController.cs
+++ przepych/Assets/Scripts/MainMenuController.cs
@@ -2,16 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-    public void StartGame()
+    public GameObject startGame;
+    public GameObject quitGame;
+
+    private void Start()
     {
-        SceneManager.LoadScene("GameScene");
+        AddEventListeners();
     }
-    public void QuitGame()
+    public void AddEventListeners()
     {
-        Debug.Log("Zamknieto gre");
+        Button startGameButton = this.startGame.GetComponent<Button>();
+        startGameButton.onClick.AddListener(HandleStartGame);
+
+        Button quitGameButton = this.quitGame.GetComponent<Button>();
+        quitGameButton.onClick.AddListener(HandleQuitGame);
+    }
+
+    public void HandleStartGame()
+    {
+        Debug.Log("Start game");
+        SceneManager.LoadScene(1);
+
+    }
+    public void HandleQuitGame()
+    {
+        Debug.Log("Quit gre");
         Application.Quit();
     }
 }
