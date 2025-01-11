808d2230f43c44f5464d70f0de41c1552a43d98e
GasiorDDL <glondzio123@gmail.com>
2025-01-11 18:25:21 +0100

    [prep15.2]Added gpu agent handling in playing

--- SplendorConsole/server3a.py
+++ SplendorConsole/server3a.py
@@ -2,16 +2,24 @@ import asyncio
 import websockets
 import json
 import numpy as np
-from dqnAgent import DQNAgent
+#from dqnAgent import DQNAgent
+from agent_gpu import DQNAgent
+import torch
 
 STATE_DIM = 348
 ACTION_DIM = 43
-agent = DQNAgent(state_dim=STATE_DIM, action_dim=ACTION_DIM)
+gra1000 = 0
+#agent = DQNAgent(state_dim=STATE_DIM, action_dim=ACTION_DIM)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+agent = DQNAgent(state_dim=STATE_DIM, action_dim=ACTION_DIM, device=device)
+previous_state = None
 
 async def handle_connection(websocket: websockets.WebSocketServerProtocol, path: str) -> None:
 
     try:
         async for message in websocket:
+            
+            global gra1000
 
             data = json.loads(message)
             id = data.get("Id")
@@ -23,6 +31,7 @@ async def handle_connection(websocket: websockets.WebSocketServerProtocol, path:
                 if len(state) != STATE_DIM:
                     return "Invalid state dimension", 400
                 
+                #TRENING
                 output = agent.generate_action_values(state)
                 
                 response_object = {
@@ -33,7 +42,17 @@ async def handle_connection(websocket: websockets.WebSocketServerProtocol, path:
 
             elif id == 2:
 
+                gra1000 += 1 
+
+                #if gra1000 % 100 == 0:
+                #    agent.save_checkpoint("./checkpoints/agent_0_checkpoint.pth")
+
+
+                # REQUEST Z ZAKOŃCZONEJ GRY Z WYGRANYM
+                #last_feedback = data.get("LastFeedback")
                 print(f"[Python] Serwer odebrał wygraną")
+                
+                #TRENING
 
                 response_object = {
                     "ResponseCode": 0,
